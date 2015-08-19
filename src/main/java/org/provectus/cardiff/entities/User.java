1aca12d3bbadff612951161ce629649980730c7f
Vlasov Artem <vlasovartem21@gmail.com>
2015-08-19 15:55:21 +0300

    Init commit. Create first entity User and basic fields

---
+++ src/main/java/org/provectus/cardiff/entities/User.java
@@ -0,0 +1,79 @@
+package org.provectus.cardiff.entities;
+
+/**
+ * Created by artemvlasov on 19/08/15.
+ */
+public class User {
+    private long id;
+    private String name;
+    private String login;
+    private String password;
+    private String email;
+    private long phoneNumber;
+    private String description;
+
+    public User() {
+    }
+
+    public User(String name, String login, String password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
