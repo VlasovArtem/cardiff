1be98dc4295fb11e4fd6094c678ff74446eca9ac
Vlasov Artem <avlasov@selectica.com>
2015-12-29 16:30:25 +0200

    Update sql patch

--- src/main/resources/DBSchema/patch/cardiff_patch_6.sql
+++ src/main/resources/DBSchema/patch/cardiff_patch_6.sql
@@ -2,4 +2,5 @@
 --- Skype credentials
 ---
 ALTER TABLE person ADD COLUMN skype VARCHAR(32) UNIQUE;
---- ALTER TABLE person ALTER skype SET NOT NULL
+-- UPDATE person SET skype = "Please add skype";
+-- ALTER TABLE person ALTER skype SET NOT NULL
